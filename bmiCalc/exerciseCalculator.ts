5c7316348e5d2e1661ce9cc5bf34868abbcf6c33
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-09-24 21:29:51 +0300

    tehtävät 9.1 - 9.3 tehty

---
+++ bmiCalc/exerciseCalculator.ts
@@ -0,0 +1,96 @@
+interface returnObj {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = ( exerciseRoutine: Array<number>, target: number ) => {
+    console.log('exerciseRoutine :>> ', exerciseRoutine);
+
+    const periodLength = exerciseRoutine.length
+
+    let trainingDays = 0
+    let hoursTrained = 0
+    for (let i = 0; i < exerciseRoutine.length; i++) {
+        const exerciseOnCurrDay = exerciseRoutine[i];
+        if(exerciseOnCurrDay > 0){
+            trainingDays++
+            hoursTrained += exerciseOnCurrDay
+        }
+    }
+
+    const traininDaysRatio = trainingDays/periodLength
+
+    const average = hoursTrained / trainingDays
+
+    let success = false
+    if(traininDaysRatio > 0.33 && average >= 1){
+        success = true
+    }
+
+    let rating = 0
+    let ratingDescription = ''
+    if(success && target < average){
+        rating = 3
+        ratingDescription = 'Very good!'
+    } else if(success){
+        rating = 2
+        ratingDescription = 'Good going, but could do better!'
+    } else {
+        rating = 1
+        ratingDescription = 'Try better next time! :('
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+console.log('exer :>> ', calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+
+interface exerciseCalculationValues {
+    target: number;
+    exerciseRoutine: Array<number>;
+}
+
+const parseExerciseArguments = (args: Array<string>): exerciseCalculationValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    const targetANumber = !isNaN(Number(args[2])) ? true : false
+
+    let exerciseRoutine = []
+    for (let i = 3; i < args.length; i++) {
+        const argument = args[i];
+        
+        if( isNaN(Number(argument)) ){
+            throw new Error('Provided values were not numbers!');
+        }
+        exerciseRoutine.push(Number(argument))
+    }
+
+    if (targetANumber) {
+        return {
+            target: Number(args[2]),
+            exerciseRoutine
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+try {
+    const { target, exerciseRoutine } = parseExerciseArguments(process.argv)
+    console.log(calculateExercises(exerciseRoutine, target));
+} catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+}
