9328d5de7818e8dd6e3c73f9cc74575b4be60015
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-10-08 07:37:30 +0300

    tehtävä 9.16 tehty

--- patientor_backend/src/routes/patientsRouter.ts
+++ patientor_backend/src/routes/patientsRouter.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import { patientGuard } from "../../utils/patientguard";
 const router = express.Router();
-import { getPatientsNoSsn, addPatient } from '../services/patientsService';
+import { getPatientsNoSsn, addPatient, getPatients } from '../services/patientsService';
+import { Patient } from '../types';
+
+type FoundPatient = Patient | undefined;
+
+router.get('/:id', (reg, res) => {
+    const id = reg.params.id;
+    const patients = getPatients();
+    let foundPatient: FoundPatient =  patients.find(patient => patient.id == id);
+
+    if(foundPatient && !foundPatient.entries){
+        foundPatient.entries = [];
+    }
+
+    res.json(foundPatient);
+});
 
 router.get('/', (_reg, res) => {
     const patients = getPatientsNoSsn();
