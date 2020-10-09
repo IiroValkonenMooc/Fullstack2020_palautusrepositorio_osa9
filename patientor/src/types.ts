ec1ceb52ad7db10bad6fc6eb60c7fe12f7810d15
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-10-09 23:33:19 +0300

    entry tyypit

--- patientor/src/types.ts
+++ patientor/src/types.ts
@@ -26,9 +26,45 @@ export interface PatientMoreInfo {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
-  entries: {
+  entries: Array<OccupationalHealthCareEntry | HospitalEntry>
+}
 
+interface HealthCareEntryBase {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+  description: string;
+}
+
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+export interface OccupationalHealthCareEntry extends HealthCareEntryBase {
+  employerName: string;
+  sickLeave?: {
+      startDate: string,
+      endDate: string,
   };
+} 
+
+export interface HospitalEntry extends HealthCareEntryBase {
+  discharge?: {
+      date: string,
+      criteria: string,
+  };
+  healthCheckRating?: HealthCheckRating;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
 }
 
 export interface RouteMatchId {
